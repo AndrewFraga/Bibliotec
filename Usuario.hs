e67dbad4f3d1166febf208f7d5d9e18e6098d96c
mateusvilione <mateusvilione@gmail.com>
2017-06-13 21:30:07 +0000

    UsuarioById - PATCH

--- Usuario.hs
+++ Usuario.hs
@@ -31,3 +31,21 @@ getBuscarUsuarioR pesquisa = do
     lu <- runDB $ selectList [ UsuarioNome %=. pesquisa ] []
     listaUsuario <- return $ Prelude.map (\( Entity i (Usuario n e _ g d) ) -> Entity i $ Usuario n e "" g d ) lu
     sendStatusJSON ok200 $ object [ "resp" .= listaUsuario ]
+
+
+-- POST ------------------------------------------------------------------------
+postUsuarioR :: Handler Value
+postUsuarioR = do
+    usuario   <- requireJsonBody :: Handler Usuario
+    usuarioId <- runDB $ insert usuario
+    sendStatusJSON created201 $ object [ "resp" .= ( fromSqlKey usuarioId ) ]
+
+-- PATCH -----------------------------------------------------------------------
+patchUsuarioByIdR :: UsuarioId -> Handler Value
+patchUsuarioByIdR usuarioId = do
+    (nome, genero, dataNascimento) <- requireJsonBody :: Handler ( Text, GeneroId, Day ) 
+    runDB $ update usuarioId [ UsuarioNome           =. nome
+                             , UsuarioGenero         =. genero
+                             , UsuarioDataNascimento =. dataNascimento
+                             ]
+    sendStatusJSON noContent204 emptyObject
